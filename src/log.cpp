d4b0b0b1ac5e09543874afc40d430980d57b4505
Jeffrey Minton <jeffrey.minton@gmail.com>
2019-03-24 16:56:12 -0700

    Add lcd logging and move logging to separate class

---
+++ src/log.cpp
@@ -0,0 +1,215 @@
+#include "log.h"
+
+
+
+
+
+Log::Log(Adafruit_LiquidCrystal lcd_ref) : lcd(lcd_ref) {
+    heartbeat_interval = 1000;
+    heartbeat_on = false;
+    heartbeat_durration = 10;
+    heartbeat_on_at = 0;
+    heartbeat_off_at = 0;
+    
+    lcd = lcd_ref;
+    // set up the LCD's number of rows and columns: 
+    lcd.begin(20, 4);
+    lcd.setBacklight(1);
+
+    current_row = 0;
+    max_row = 3;
+
+    message_num = 0;
+}
+
+
+int Log::freeMemory()
+{
+    char top;
+#ifdef __arm__
+    return &top - reinterpret_cast<char *>(sbrk(0));
+#elif defined(CORE_TEENSY) || (ARDUINO > 103 && ARDUINO != 151)
+    return &top - __brkval;
+#else  // __arm__
+    return __brkval ? &top - __brkval : &top - __malloc_heap_start;
+#endif // __arm__
+}
+
+
+
+void Log::print_log_pre(bool force, bool heartbeat){
+    // print_log_msg.remove(0);
+    Serial.print("Time: ");
+    Serial.print(millis());
+    Serial.print(", Free Mem: ");
+    Serial.print(freeMemory());
+    Serial.print(", MSG: ");
+    if(force == true){
+        Serial.print(", Forced: ");
+    } else if(heartbeat == true){
+        Serial.print(", Heartbeat: ");
+    }
+}
+
+void Log::print_log(char* msg, bool force, bool heartbeat){
+    print_log_pre(force, heartbeat);
+    Serial.println(msg);
+}
+
+void Log::print_log(int msg, bool force, bool heartbeat){
+    print_log_pre(force, heartbeat);
+    Serial.println(msg);
+}
+
+void Log::print_log(byte msg, bool force, bool heartbeat){
+    print_log_pre(force, heartbeat);
+    Serial.println(msg);
+}
+
+
+
+void Log::check_heartbeat() {
+    if(heartbeat_on_at == 0) {
+        // Serial.println('Heartbeat is None')
+        heartbeat_on_at = millis() + heartbeat_interval;
+        heartbeat_off_at = heartbeat_on_at + heartbeat_durration;
+    }
+    else if(millis() >= heartbeat_off_at) {
+        heartbeat_on = false;
+        //At the end of the pi heartbeat interval get the teensy logs
+        heartbeat_on_at = millis() + heartbeat_interval;
+        heartbeat_off_at = heartbeat_on_at + heartbeat_durration;
+        // Serial.println('heartbeat off')
+    }
+    else if(millis() >= heartbeat_on_at) {
+        // Serial.println('heartbeat on')
+        heartbeat_on = true;
+        // print_log("Heartbeat: ".concat(log_msg));
+    }
+}
+
+
+void Log::heartbeat_log(char* log_msg, bool force) {
+        // Serial.println('millis: %s, on at: %s, off at: %s' % (str(millis()), str(heartbeat_on_at), str(heartbeat_off_at)))
+        if(force == true){
+            print_log(log_msg, force, false);
+        }
+        check_heartbeat();
+        if(heartbeat_on == true) {
+            // print_log(log_msg, false, true);
+        }
+}
+
+
+void Log::heartbeat_log(int log_msg, bool force) {
+        // Serial.println('millis: %s, on at: %s, off at: %s' % (str(millis()), str(heartbeat_on_at), str(heartbeat_off_at)))
+        if(force == true){
+            print_log(log_msg, force, false);
+        }
+        check_heartbeat();
+        if(heartbeat_on == true) {
+            // print_log(log_msg, false, true);
+        }
+}
+
+void Log::heartbeat_log(byte log_msg, bool force) {
+        // Serial.println('millis: %s, on at: %s, off at: %s' % (str(millis()), str(heartbeat_on_at), str(heartbeat_off_at)))
+        if(force == true){
+            print_log(log_msg, force, false);
+        }
+        check_heartbeat();
+        if(heartbeat_on == true) {
+            // print_log(log_msg, false, true);
+        }
+}
+
+
+void Log::heartbeat_log(char log_msg, bool force) {
+    heartbeat_log(&log_msg, force);
+}
+
+
+void Log::heartbeat_log(char* msg){
+    heartbeat_log(msg, false);
+}
+
+void Log::heartbeat_log(char msg){
+    heartbeat_log(&msg, false);
+}
+
+void Log::heartbeat_log(int msg){
+    heartbeat_log(msg, false);
+}
+
+void Log::heartbeat_log(byte msg){
+    heartbeat_log(msg, false);
+}
+
+
+
+
+void Log::print_lcd_pre() {
+    if( current_row > max_row ) {
+        current_row = 0;
+    }
+    lcd.setCursor(0, current_row);
+    lcd.print("                    ");
+    lcd.setCursor(0, current_row);
+    lcd.print(message_num);
+    lcd.print(":");
+    message_num++;
+}
+
+
+void Log::print_lcd(char* msg, bool new_line){
+    if(new_line == true) {
+        print_lcd_pre();
+    }
+    lcd.print(msg);
+    current_row++;
+}
+
+void Log::print_lcd(char msg, bool new_line){
+    if(new_line == true) {
+        print_lcd_pre();
+    }
+    lcd.print(msg);
+    current_row++;
+}
+
+void Log::print_lcd(int msg, bool new_line){
+    if(new_line == true) {
+        print_lcd_pre();
+    }
+    lcd.print(msg);
+    current_row++;
+}
+
+void Log::print_lcd(byte msg, bool new_line){
+    if(new_line == true) {
+        print_lcd_pre();
+    }
+    lcd.print(msg);
+    current_row++;
+}
+
+
+void Log::lcd_log(char* log_msg, bool new_line) {
+    print_lcd(log_msg, new_line);
+}
+
+
+void Log::lcd_log(int log_msg, bool new_line) {
+    print_lcd(log_msg, new_line);
+}
+
+void Log::lcd_log(byte log_msg, bool new_line) {
+    print_lcd(log_msg, new_line);
+}
+
+
+void Log::lcd_log(char log_msg, bool new_line) {
+    print_lcd(log_msg, new_line);
+}
+
+
---
+++ src/log.h
@@ -0,0 +1,87 @@
+#ifndef LOG_H
+#define LOG_H
+
+#include <Arduino.h>
+#include "Adafruit_LiquidCrystal.h"
+
+
+#ifdef __arm__
+// should use uinstd.h to define sbrk but Due causes a conflict
+extern "C" char *sbrk(int incr);
+#else  // __ARM__
+extern char *__brkval;
+#endif // __arm__
+
+
+class Log
+{
+private:
+    uint8_t lcd_latch_pin;
+    uint8_t lcd_clock_pin;
+    uint8_t lcd_data_pin;
+    unsigned long heartbeat_interval;
+    bool heartbeat_on;
+    int heartbeat_durration;
+    unsigned long heartbeat_on_at;
+    unsigned long heartbeat_off_at;
+    Adafruit_LiquidCrystal lcd;
+    int current_row;
+    int max_row;
+    int message_num;
+ 
+public:
+
+    // Date(int year, int month, int day)
+    // {
+    //     setDate(year, month, day);
+    // }
+
+    Log(Adafruit_LiquidCrystal lcd);
+
+    // void setDate(int year, int month, int day)
+    // {
+    //     m_year = year;
+    //     m_month = month;
+    //     m_day = day;
+    // }
+ 
+    // int getYear() { return m_year; }
+    // int getMonth() { return m_month; }
+    // int getDay()  { return m_day; }
+
+    int freeMemory();
+
+    void check_heartbeat() ;
+
+
+    void print_log_pre(bool force, bool heartbeat);
+    void print_log(char* msg, bool force, bool heartbeat);
+    void print_log(int msg, bool force, bool heartbeat);
+    void print_log(byte msg, bool force, bool heartbeat);
+    void heartbeat_log(char* log_msg, bool force);
+    void heartbeat_log(int log_msg, bool force) ;
+    void heartbeat_log(byte log_msg, bool force);
+    void heartbeat_log(char log_msg, bool force);
+    void heartbeat_log(char* msg);
+    void heartbeat_log(char msg);
+    void heartbeat_log(int msg);
+    void heartbeat_log(byte msg);
+
+
+    void print_lcd_pre();
+    void print_lcd(char* msg, bool new_line);
+    void print_lcd(char msg, bool new_line);
+    void print_lcd(int msg, bool new_line);
+    void print_lcd(byte msg, bool new_line);
+    void lcd_log(char* log_msg, bool new_line = true);
+    void lcd_log(int log_msg, bool new_line = true);
+    void lcd_log(byte log_msg, bool new_line = true);
+    void lcd_log(char log_msg, bool new_line = true);
+    // void lcd_log(char* log_msg);
+    // void lcd_log(int log_msg);
+    // void lcd_log(byte log_msg);
+    // void lcd_log(char log_msg);
+
+};
+
+#endif
